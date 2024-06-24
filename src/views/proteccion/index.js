8909aa40fe8ede4b5eda0bc4ad890019c2b2e92a
Uprocach <bi.uprocach@gmail.com>
2024-06-24 18:10:53 -0500

    Actulizacion link manual-proteccion

--- src/views/proteccion/index.js
+++ src/views/proteccion/index.js
@@ -13,9 +13,9 @@ const Proteccion = () => {
     <>
       <Grid container spacing={gridSpacing}>
         <Grid item xs={12}>
-          <div style={{ width: '100%', height: '60vh' }}>
+          <div style={{ width: '100%', height: '144vh' }}>
             <iframe
-              src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGJFvZj_jo&#x2F;up_LwsJAeOMsa2-ZvLXt2Q&#x2F;view?embed"
+              src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGJFiDTSyE&#x2F;PUVZUohBXsZDz52nYHAswg&#x2F;view?embed"
               allowFullScreen="true"
               style={{ width: '100%', height: '100%', border: 'none', borderRadius: '15px' }}
               title="Operaciones Concedidas"
