2f0ae84e061570dfd1473bbb0ce9f0ceec5b3c11
Uprocach <bi.uprocach@gmail.com>
2024-06-25 10:59:10 -0500

    Udpate file manual-proteccion

--- src/views/manual/index.js
+++ src/views/manual/index.js
@@ -14,12 +14,11 @@ const Manual = () => {
       <Grid container spacing={gridSpacing}>
         <Grid item xs={12}>
           <div style={{ width: '100%', height: '100vh' }}>
-            <iframe
-              src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGJFwg-uuc&#x2F;v4mrvBjJVnd3ug_ed4rqvw&#x2F;view?embed"
-              allowFullScreen="true"
-              style={{ width: '100%', height: '720px', border: 'none', borderRadius: '15px' }}
-              title="Operaciones Concedidas"
-            ></iframe>
+            <img
+              src={require('../../assets/images/manual/manual.png')}
+              alt="Proteccion Datos"
+              style={{ width: '100%', height: 'auto', borderRadius: '15px' }}
+            />
           </div>
         </Grid>
       </Grid>
