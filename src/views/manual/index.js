78a2a126379cfec78c9b580b9b89a371bffe0c77
Uprocach <bi.uprocach@gmail.com>
2024-06-24 19:27:51 -0500

    Cambio enlaces manual-proteccion

--- src/views/manual/index.js
+++ src/views/manual/index.js
@@ -13,11 +13,11 @@ const Manual = () => {
     <>
       <Grid container spacing={gridSpacing}>
         <Grid item xs={12}>
-          <div style={{ width: '100%', height: '58vh' }}>
+          <div style={{ width: '100%', height: '100vh' }}>
             <iframe
-              src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGJFiSfzQQ&#x2F;CFiC2RURSv8QM2N9zwQvow&#x2F;view?embed"
+              src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGJFwg-uuc&#x2F;v4mrvBjJVnd3ug_ed4rqvw&#x2F;view?embed"
               allowFullScreen="true"
-              style={{ width: '100%', height: '100%', border: 'none', borderRadius: '15px' }}
+              style={{ width: '100%', height: '720px', border: 'none', borderRadius: '15px' }}
               title="Operaciones Concedidas"
             ></iframe>
           </div>
--- src/views/proteccion/index.js
+++ src/views/proteccion/index.js
@@ -13,11 +13,11 @@ const Proteccion = () => {
     <>
       <Grid container spacing={gridSpacing}>
         <Grid item xs={12}>
-          <div style={{ width: '100%', height: '144vh' }}>
+          <div style={{ width: '100%', height: '100vh' }}>
             <iframe
               src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGJFiDTSyE&#x2F;PUVZUohBXsZDz52nYHAswg&#x2F;view?embed"
               allowFullScreen="true"
-              style={{ width: '100%', height: '100%', border: 'none', borderRadius: '15px' }}
+              style={{ width: '100%', height: '100%', border: 'none', borderRadius: '15px', fill: 'cover' }}
               title="Operaciones Concedidas"
             ></iframe>
           </div>
